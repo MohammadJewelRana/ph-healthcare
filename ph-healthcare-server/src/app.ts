23314d0312ce7e6592a7d9636a3768ddd939ee9d
MohammadJewelRana <js.rana0326@gmail.com>
2025-06-21 18:56:00 +0600

    server code organized and app.ts create

---
+++ ph-healthcare-server/src/app.ts
@@ -0,0 +1,47 @@
+
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+ 
+ 
+
+const app: Application = express();
+app.use(cors());
+ 
+
+//parser
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+
+// cron.schedule('* * * * *', () => {
+//     try {
+//         AppointmentService.cancelUnpaidAppointments();
+//     }
+//     catch (err) {
+//         console.error(err);
+//     }
+// });
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "Ph health care server.."
+    })
+});
+
+// app.use('/api/v1', router);
+
+// app.use(globalErrorHandler);
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     res.status(httpStatus.NOT_FOUND).json({
+//         success: false,
+//         message: "API NOT FOUND!",
+//         error: {
+//             path: req.originalUrl,
+//             message: "Your requested path is not found!"
+//         }
+//     })
+// })
+
+export default app;
--- ph-healthcare-server/src/server.ts
+++ ph-healthcare-server/src/server.ts
@@ -1,43 +1,37 @@
 
-// import { Server } from 'http';
+import { Server } from 'http';
+import app from './app';
  
-
-
-
-// async function main() {
-//     const server: Server = app.listen(config.port, () => {
-//         console.log("Sever is running on port ", config.port);
-//     });
-
-//     const exitHandler = () => {
-//         if (server) {
-//             server.close(() => {
-//                 console.info("Server closed!")
-//             })
-//         }
-//         process.exit(1);
-//     };
-//     process.on('uncaughtException', (error) => {
-//         console.log(error);
-//         exitHandler();
-//     });
-
-//     process.on('unhandledRejection', (error) => {
-//         console.log(error);
-//         exitHandler();
-//     })
-// };
-
-// main();
-
-
-
-import express from 'express'
-const app=express();
 
 const port=3000;
 
-app.listen(port,()=>{
-    console.log('App is listening on port ', port);
-    
-})
+async function main() {
+    const server: Server = app.listen( port, () => {
+    // const server: Server = app.listen(config.port, () => {
+        console.log("Sever is running on port ",  port);
+        // console.log("Sever is running on port ", config.port);
+    });
+
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server closed!")
+            })
+        }
+        process.exit(1);
+    };
+    process.on('uncaughtException', (error) => {
+        console.log(error);
+        exitHandler();
+    });
+
+    process.on('unhandledRejection', (error) => {
+        console.log(error);
+        exitHandler();
+    })
+};
+
+main();
+
+
+
