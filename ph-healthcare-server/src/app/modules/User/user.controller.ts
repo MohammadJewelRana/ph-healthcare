f10ddc9931d41274f885ad48a9a9c51d8257300c
MohammadJewelRana <js.rana0326@gmail.com>
2025-06-22 16:00:46 +0600

    admin modules

--- ph-healthcare-server/src/app/modules/User/user.controller.ts
+++ ph-healthcare-server/src/app/modules/User/user.controller.ts
@@ -6,6 +6,8 @@ import { userService } from "./user.service";
 
 const createAdmin = catchAsync(async (req: Request, res: Response) => {
 
+    console.log(req.body);
+    
     const result = await userService.createAdmin(req);
     sendResponse(res, {
         statusCode: httpStatus.OK,
--- ph-healthcare-server/src/app/modules/User/user.service.ts
+++ ph-healthcare-server/src/app/modules/User/user.service.ts
@@ -1,25 +1,48 @@
-import { Admin, Doctor, Patient, Prisma, User, UserRole, UserStatus } from "@prisma/client";
-// import * as bcrypt from 'bcrypt'
-
+import {
+  Admin,
+  Doctor,
+  Patient,
+  Prisma,
+  User,
+  UserRole,
+  UserStatus,
+} from "@prisma/client";
+import * as bcrypt from 'bcrypt'
 
 import { Request } from "express";
-
+import prisma from "../../../shared/prisma";
 
 const createAdmin = async (req: Request) => {
 
-   
+    const hashedPassword: string = await bcrypt.hash(req.body.password, 12)
 
- return null;
+  const userData = {
+    email: req.body.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
 
-};
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: req.body.admin,
+    });
+
+    return createdAdminData;
+  });
  
+  return result;
+};
 
 export const userService = {
-    createAdmin,
-    // createDoctor,
-    // createPatient,
-    // getAllFromDB,
-    // changeProfileStatus,
-    // getMyProfile,
-    // updateMyProfie
-}
+  createAdmin,
+  // createDoctor,
+  // createPatient,
+  // getAllFromDB,
+  // changeProfileStatus,
+  // getMyProfile,
+  // updateMyProfie
+};
