2fd6cdfc958e4a12457a2bbb186a64b3747fe03f
MohammadJewelRana <js.rana0326@gmail.com>
2025-06-22 13:11:22 +0600

    global error handler and otther validated utility added

--- ph-healthcare-server/src/app.ts
+++ ph-healthcare-server/src/app.ts
@@ -1,7 +1,9 @@
 
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
- 
+import router from './app/routes';
+ import httpStatus from "http-status";
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
  
 
 const app: Application = express();
@@ -10,7 +12,7 @@ app.use(cors());
 
 //parser
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -29,19 +31,19 @@ app.get('/', (req: Request, res: Response) => {
     })
 });
 
-// app.use('/api/v1', router);
+app.use('/api/v1', router);
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     res.status(httpStatus.NOT_FOUND).json({
-//         success: false,
-//         message: "API NOT FOUND!",
-//         error: {
-//             path: req.originalUrl,
-//             message: "Your requested path is not found!"
-//         }
-//     })
-// })
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success: false,
+        message: "API NOT FOUND!",
+        error: {
+            path: req.originalUrl,
+            message: "Your requested path is not found!"
+        }
+    })
+})
 
 export default app;
--- ph-healthcare-server/src/config/index.ts
+++ ph-healthcare-server/src/config/index.ts
@@ -29,3 +29,5 @@ export default {
         sslValidationApi: process.env.SSL_VALIDATIOIN_API
     }
 }
+
+ 
