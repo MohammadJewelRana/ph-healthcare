469e2ffb45f39e1430bb57a3af77a0679581c652
MohammadJewelRana <js.rana0326@gmail.com>
2025-06-22 12:52:05 +0600

    catchasync ,sendresponse and routes setup

---
+++ ph-healthcare-server/src/app/modules/User/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, RequestHandler, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+ import httpStatus from "http-status";
+import { userService } from "./user.service";
+
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await userService.createAdmin(req);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin Created successfully!",
+        data: result
+    })
+});
+
+
+export const userController = {
+    createAdmin,
+
+}
+
+ 
---
+++ ph-healthcare-server/src/app/modules/User/user.service.ts
@@ -0,0 +1,25 @@
+import { Admin, Doctor, Patient, Prisma, User, UserRole, UserStatus } from "@prisma/client";
+// import * as bcrypt from 'bcrypt'
+
+
+import { Request } from "express";
+
+
+const createAdmin = async (req: Request) => {
+
+   
+
+ return null;
+
+};
+ 
+
+export const userService = {
+    createAdmin,
+    // createDoctor,
+    // createPatient,
+    // getAllFromDB,
+    // changeProfileStatus,
+    // getMyProfile,
+    // updateMyProfie
+}
